be7bfe05bdbcab05cbdbaac93cf1dbdebfac6db6
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-02 23:51:09 +0900

    初期表示では全項目が展開された状態で、DOMContentLoadedで非表示に切り替え

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,23 +1,39 @@
 var buttonState = {
-    "text": false,
-    "html": false,
-    "html-select": false,
-    "sql": false,
-    "sql-no-id": false,
-    "json": false
+    "text": true,
+    "html": true,
+    "html-select": true,
+    "sql": true,
+    "sql-no-id": true,
+    "json": true
 };
 
-function toggle(part) {
-    var html = document.getElementById(part);
-    var btn = document.getElementById("btn-" + part);
+/**
+ * 項目の表示・非表示を切り替える
+ *
+ * @param id 表示を切り替える項目のid
+ */
+function toggle(id) {
+    var target = document.getElementById(id);
+    var button = document.getElementById("btn-" + id);
 
-    if (buttonState[part]) {
-        html.style.display = "none";
-        btn.innerText = "表示";
-    } else {
-        html.style.display = "block";
-        btn.innerText = "非表示";
+    if (!target || !button) {
+        throw new Error("指定したIDの要素がみつかりません:" + id);
     }
 
-    buttonState[part] = !buttonState[part];
+    if (buttonState[id]) {
+        target.style.display = "none";
+        button.innerText = "表示";
+    } else {
+        target.style.display = "block";
+        button.innerText = "非表示";
+    }
+
+    buttonState[id] = !buttonState[id];
 }
+
+// 各項目が初期表示で非表示になるようにする
+document.addEventListener("DOMContentLoaded", function () {
+    Object.keys(buttonState).forEach(function (id) {
+        toggle(id);
+    });
+});
