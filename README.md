86a739a448beb4100f8c5288c7c7cd1db8a9f392
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-02 20:53:53 +0900

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Japanese prefectures API
+
+日本の47都道府県のリストを返すWeb APIです。
---
+++ web/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/', function() use($app) {
+  return $app['twig']->render('index.twig');
+});
+
+$app->run();
