111cfc38682cd2349f7bd05a42653f8dac4b2a77
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-02 21:09:34 +0900

    マスタデータを追加、/json 追加

--- web/index.php
+++ web/index.php
@@ -1,22 +1,31 @@
-<?php
+<?php namespace ryo511\JapanesePrefecturesApi;
+
+use Silex\Application;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Silex\Application();
+$app = new Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+// Setup
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/views',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// Handle request
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.twig');
+});
 
-$app->get('/', function() use($app) {
-  return $app['twig']->render('index.twig');
+$app->get('/json', function () use ($app) {
+    $json = file_get_contents(__DIR__ . '/../prefectures.json');
+    return new Response($json);
 });
 
 $app->run();
